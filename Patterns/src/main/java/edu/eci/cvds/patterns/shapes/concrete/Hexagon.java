7dac5fc4b5981753623549af66fe2fe50454ede0
AngieMeG <70661112+AngieMeG@users.noreply.github.com>
2021-01-28 09:56:38 -0500

    Patterns

---
+++ Patterns/src/main/java/edu/eci/cvds/patterns/shapes/concrete/Hexagon.java
@@ -0,0 +1,13 @@
+package edu.eci.cvds.patterns.shapes.concrete;
+
+import edu.eci.cvds.patterns.shapes.Shape;
+
+/** Hexagon's implementation class
+ * @version 1.0
+ * @author Angie Medina - Jose Perez
+ */
+public class Hexagon implements Shape {
+    public int getNumberOfEdges() {
+        return 6;
+    }
+}
